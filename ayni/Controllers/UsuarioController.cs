0defcafee60ee075efa7a90159e74f35727474cc
leonmanu <leonmanu@gmail.com>
2019-09-17 01:19:19 -0300

    Registro, sólo alta

---
+++ ayni/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using ayni.Models;
+using ayni.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ayni.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        UsuarioService usuarioService = new UsuarioService();
+        // GET: Usuario
+        public ActionResult Registro()
+        {
+            ViewBag.Message = "Regístrese en esta sección.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Usuario usuario)
+        {
+            //if (ModelState.IsValid)
+           // {
+                if (usuarioService.Alta(usuario))
+                {
+                    TempData["RegistroMsj"] = "<p class='mb-0 text-success'> El usuario se registró correctamente </p>";
+                }
+                else
+                {
+                    TempData["RegistroMsj"] = "<p class='mb-0 text-danger'> No se pudo registrar el usuario </p>";
+                }
+           // }
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+    }
+}
---
+++ ayni/Services/UsuarioService.cs
@@ -0,0 +1,22 @@
+﻿using ayni.Models;
+using ayni.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ayni.Services
+{
+    public class UsuarioService
+    {
+        ProyectoAyniEntities ctx = new ProyectoAyniEntities();
+
+        //Alta
+        public bool Alta(Usuario usuario)
+        {
+            ctx.Usuario.Add(usuario);
+            ctx.SaveChanges();
+            return true;
+        }
+    }
+}
