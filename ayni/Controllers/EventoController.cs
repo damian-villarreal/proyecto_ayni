cedfd3352d7c01001a065668baaa3b3f56893e13
leonmanu <leonmanu@gmail.com>
2019-11-17 22:54:14 -0300

    select en cascada para la ubicación

---
+++ ayni/Controllers/EventoController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Data;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Web.Script.Serialization;
+
+namespace ayni.Controllers
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+
+        public string CargaJsonProvincia()
+        {
+            string path = "https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.2/download/provincias.json";
+
+            //string provinciaJson = System.IO.File.ReadAllText(path, Encoding.UTF8);
+            var webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+
+            var provinciaJson = (webClient).DownloadString(path);
+            JObject rss = JObject.Parse(provinciaJson);
+            var provinciaFiltro = from p in rss["provincias"] orderby p["id"] select p;
+            var jsonToOutput = JsonConvert.SerializeObject(provinciaFiltro, Formatting.Indented);
+            return jsonToOutput;
+        }
+
+        /*
+         public JsonResult CargaJson()
+        {
+            string path = @"C:\Users\Manu\source\repos\ayni\ayni\Scripts\provincias.json";
+
+            string provinciaJson = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            System.Diagnostics.Debug.WriteLine("Entró a CargaJsons: " + provinciaJson);
+            return Json(provinciaJson);
+        }
+        */
+        /*
+        public List<string> CargaJson()
+        {
+            System.Diagnostics.Debug.WriteLine("Entró a CargaJsons: ");
+            string path = @"C:\Users\Manu\source\repos\ayni\ayni\Scripts\provincias.json";
+
+            string provinciaJson = System.IO.File.ReadAllText(path);
+            List<string> provinciaList = JsonConvert.DeserializeObject<List<string>>(provinciaJson.provincias);
+
+            // List<string> videogames = JsonConvert.DeserializeObject<List<string>>(json);
+            System.Diagnostics.Debug.WriteLine(string.Join(", ", provinciaList.ToArray()));
+            //System.Diagnostics.Debug.WriteLine("provinciaList: " + provinciaList.ToArray());
+            return (provinciaList);
+        }*/
+
+        public string CargaJsonMunicipio(string name)
+        {
+            string path = "https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.5/download/localidades.json";
+
+            //string path = "https://infra.datos.gob.ar/catalog/modernizacion/dataset/7/distribution/7.4/download/municipios.json";
+
+            System.Diagnostics.Debug.WriteLine("ID Provincia: " + name);
+            var webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+
+            var municipioUrl = (webClient).DownloadString(path);
+            var municipioJson = JsonConvert.DeserializeObject(municipioUrl);
+
+            JObject rss = JObject.Parse(municipioUrl);
+            var provinciaFiltro = from m in rss["localidades"] orderby m["nombre"] select m;
+
+            //var jsonToOutput = JsonConvert.SerializeObject(provinciaFiltro, Formatting.Indented);
+
+            JArray municipalidadArray = new JArray();
+            //array.Add(Itemid);
+
+            foreach (var item in provinciaFiltro)
+            {
+                if (item["provincia"]["id"].ToString() == name)
+                {
+                    System.Diagnostics.Debug.WriteLine("Provincia: " + item["provincia"]["nombre"]);
+                    municipalidadArray.Add(item);
+                }
+
+            }
+
+            foreach (var item in municipalidadArray)
+            {
+                System.Diagnostics.Debug.WriteLine("municipio: " + item);
+                //array.Add(item["nombre"]);
+
+            }
+
+            var municipalidadjsonToOutput = JsonConvert.SerializeObject(municipalidadArray, Formatting.Indented);
+
+            return municipalidadjsonToOutput;
+        }
+    }
+}   
---
+++ ayni/Scripts/eventos/evento-ubicacion.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+
+    //PROVINCIAS
+    console.log("entro a lugares.js");
+    $.ajax({
+        url: '/Evento/CargaJsonProvincia',
+        contentType: 'application/json',
+        method: 'POST',
+        //data: JSON.stringify({ name: selector }),
+        success: function (response) {
+            var jsonResponser = JSON.parse(response);
+            var slcProvi = $('#Provincia');
+            slcProvi.html('');
+            slcProvi.append("<option value='' selected='' disabled=''>Seleccionar</option>\ ");
+            jsonResponser.forEach(function (p) {
+                slcProvi.append('<option value=' + p.id + ' >' + p.nombre + '</option>\ ');
+                console.log(p.nombre);
+            })
+        }
+    });
+
+
+    //MUNICIPIOS
+    console.log("entro a dISTRITOS.js");
+    $('#Provincia').on('change', function () {
+        var selector = $(this).val();
+        $("#wait").css("display", "block");
+        $.ajax({
+            url: '/Evento/CargaJsonMunicipio',
+            contentType: 'application/json',
+            method: 'POST',
+            data: JSON.stringify({ name: selector }),
+            success: function (response) {
+                var jsonResponser = JSON.parse(response);
+                var slcMunicipio = $('#Municipio');
+                slcMunicipio.html('');
+                slcMunicipio.removeAttr('disabled');
+                slcMunicipio.attr('selected', false);
+                slcMunicipio.append("<option value='' selected='' disabled=''>Seleccionar</option>\ ");
+
+                jsonResponser.forEach(function (m) {
+                    slcMunicipio.append('<option value=' + m.id + ' >' + m.nombre + '</option>\ ');
+                    console.log(m.nombre);
+                })
+                $("#wait").css("display", "none");
+            }
+        });
+    });
+}); 
